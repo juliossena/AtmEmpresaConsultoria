1b5c8bafcedbbd7a0f5c08051524e1f6ce838995
juliossena <juliosouzasena@gmail.com>
2018-06-23 18:17:46 -0300

    final aplicativo

--- app/src/main/java/atmempresaconsultoria/cursoandroid/dell/atmempresaconsultoria/MainActivity.java
+++ app/src/main/java/atmempresaconsultoria/cursoandroid/dell/atmempresaconsultoria/MainActivity.java
@@ -1,13 +1,57 @@
 package atmempresaconsultoria.cursoandroid.dell.atmempresaconsultoria;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private ImageView imgEmpresa;
+    private ImageView imgContato;
+    private ImageView imgServicos;
+    private ImageView imgClientes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgEmpresa = (ImageView) findViewById(R.id.empresa_id);
+        imgContato = (ImageView) findViewById(R.id.contato_id);
+        imgServicos = (ImageView) findViewById(R.id.servicos_id);
+        imgClientes = (ImageView) findViewById(R.id.clientes_id);
+
+        imgEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, EmpresaActivity.class));
+            }
+        });
+
+        imgContato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ContatoActivity.class));
+            }
+        });
+
+        imgServicos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ServicosActivity.class));
+            }
+        });
+
+        imgClientes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ClienteActivity.class));
+            }
+        });
     }
+
+
 }
